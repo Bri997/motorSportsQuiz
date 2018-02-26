5cd0735d593fd8407aa6e095116589ab97e06b57
Brian <34427128+Bri997@users.noreply.github.com>
2018-02-26 10:21:30 -0800

    Fixed typos added aria

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const questionList = [
 
   {
 
-  question: 'The 24 Hours of Lemans first race was in what year?',
+  question: 'The 24 Hours of Lemans first race was in what year ?',
   answer1: '1951',
   answer2: '1923',
   answer3: '1946',
@@ -21,7 +21,7 @@ const questionList = [
 
   {
 
-  question: 'The Mille Miglia an open-road endurance race took place from 1927 to 1957. Where was the race held?',
+  question: 'The Mille Miglia an open-road endurance race took place from 1927 to 1957. Where was the race held ?',
   answer1: 'Sardegna',
   answer2: 'Spain',
   answer3: 'Italy',
@@ -31,8 +31,8 @@ const questionList = [
 
   {
 
-  question: 'The Brabham BT46B Formula 1 car was banned just after one race for having what?',
-  answer1: 'llegal side skirts',
+  question: 'The Brabham BT46B Formula 1 car was banned just after one race for having what ?',
+  answer1: 'illegal side skirts',
   answer2: 'Wider tyers',
   answer3: 'High capacity fuel tank ',
   answer4: 'A big fan',
@@ -41,62 +41,62 @@ const questionList = [
 
   {
 
-  question: 'The fastest top speed down the mulsanne straight at Le Mans is?',
+  question: 'The fastest top speed down the mulsanne straight at Le Mans is ?',
   answer1: '251 MPH',
   answer2: '188 MPH',
   answer3: '265 MPH',
   answer4: '199 MPH',
-  correct: '251'
+  correct: '251 MPH'
   },
 
   {
 
-    question: 'The constructor with the most wins in Formula 1 is ?',
-    answer1: 'Ferrari',
-    answer2: 'Williams',
-    answer3: 'Lotus',
-    answer4: 'Red Bull',
-    correct: 'Ferrari'
+    question: 'The last turn of the last race determined the Formula 1 drivers championship in what year ?',
+    answer1: '2006',
+    answer2: '1999',
+    answer3: '2008',
+    answer4: '2011',
+    correct: '2008'
   },
 
   {
 
-  question: 'The 24 Hours of Lemans first race was in what year?',
-  answer1: '1951',
-  answer2: '1923',
-  answer3: '1946',
-  answer4: '1939',
-  correct: '1923'
+  question: 'In Formula 1 James Hunt was know for ?',
+  answer1: 'Calculated  driving style',
+  answer2: 'Aggressive track driving',
+  answer3: 'Extreme partying',
+  answer4: 'Winning the most races',
+  correct: 'Extreme partying'
+
+},
+  {
+
+  question: 'The last year for the classic running start of the 24 Hours of LeMans ?',
+  answer1: '1969',
+  answer2: '1955',
+  answer3: '1975',
+  answer4: '1961',
+  correct: '1969'
   },
 
   {
 
-  question: 'The Mille Miglia an open-road endurance race took place from 1927 to 1957. Where was the race held?',
-  answer1: 'Sardegna',
-  answer2: 'Spain',
-  answer3: 'Italy',
-  answer4: 'Monaco',
-  correct: 'Italy'
+  question: 'The modern Formula 1 car now costs about how much ?',
+  answer1: '$957 Thousand',
+  answer2: '$100 Million',
+  answer3: '$8 Million',
+  answer4: '2 Bitcoins',
+  correct: '$8 Million'
   },
 
   {
 
-  question: 'The Brabham BT46B Formula 1 car was banned just after one race for having what?',
-  answer1: 'llegal side skirts',
-  answer2: 'Wider tyers',
-  answer3: 'High capacity fuel tank ',
-  answer4: 'A big fan',
-  correct: 'A big fan'
-  },
-
-  {
-
-  question: 'The fastest top speed down the mulsanne straight at Le Mans is?',
-  answer1: '251 MPH',
-  answer2: '188 MPH',
-  answer3: '265 MPH',
-  answer4: '199 MPH',
-  correct: '251'
+  question: 'The Nürburgring Nordschleife Combined has how many turns ?',
+  answer1: '88',
+  answer2: '117',
+  answer3: '10',
+  answer4: '170',
+  correct: '170'
   }
 
 ];
@@ -129,31 +129,31 @@ function handleStartButton() {
           <form>
             <fieldset>
             <legend>${questionList[questionNumber].question}</legend>
-              <div class="radios">
+              <div class ='radios'>
               <label for="answer1">
-                <input type="radio" name="answer" id="answer1" required>
+                <input type="radio" aria-labelledby="radio_label" name="answer" id="answer1" required>
               ${questionList[questionNumber].answer1}
               </label>
               </div>
-              <div class="radios">
+              <div class ='radios'>
               <label for="answer2">
                 <input type="radio" name="answer" id="answer2" required>
               ${questionList[questionNumber].answer2}
               </label>
               </div>
-              <div class="radios">
+              <div class ='radios'>
               <label for="answer3">
                 <input type="radio" name="answer" id="answer3" required>
               ${questionList[questionNumber].answer3}
               </label>
               </div>
-              <div class="radios">
+              <div class ='radios'>
               <label for="answer4">
                 <input type="radio" name="answer" id="answer4" required>
               ${questionList[questionNumber].answer4}
               </label>
               </div>
-              <button type="submit" class="btn btn-success submitButton">Submit</button>
+              <button type="submit" aria-labelledby="Start button" class="btn btn-dark submitButton">Submit</button>
             </fieldset>
           </form>
     `);
@@ -182,10 +182,10 @@ function handleSubmitButton() {
 
 function correctFeedback() {
   emptyContainer();
-  console.log('`correctFeedback` ran')
+  // console.log('`correctFeedback` ran')
   $('.pageInserts').append(`
     <h3>Correct! Keep going!</h3>
-    <button type="button" class="btn btn-success nextButton">Next Question</button>
+    <button type="button" aria-labelledby="Next button" class="btn btn-dark nextButton">Next Question</button>
   `);
   handleNextButton();
   addQuizScore();
@@ -194,11 +194,11 @@ function correctFeedback() {
 function incorrectFeedback() {
   let correctAnswer = `${questionList[questionNumber].correct}`;
   emptyContainer();
-  console.log('`incorrectFeedback` ran');
+  // console.log('`incorrectFeedback` ran');
   $('.pageInserts').append(`
     <h3>Woops.. </h3>
     <h3>It was ${correctAnswer}</h3>
-    <button type="button" class="btn btn-success nextButton">Next Question</button>
+    <button type="button" aria-labelledby="Next button" class="btn btn-dark nextButton">Next Question</button>
   `);
   handleNextButton();
 }
@@ -206,43 +206,43 @@ function incorrectFeedback() {
 function handleNextButton() {
   $('.nextButton').on('click', function(event) {
     if (questionNumber < 9) {
-      console.log('`handleNextButton` ran');
+      // console.log('`handleNextButton` ran');
       addQuestionNumber();
       emptyContainer();
       $('.pageInserts').append(`
           <form>
             <fieldset>
+              <div class ='radios'>
             <legend>${questionList[questionNumber].question}</legend>
-              <div class="radios">
               <label for="answer1">
-                <input type="radio" name="answer" id="answer1" required>
+                <input type="radio" aria-labelledby="radio_label" name="answer" id="answer1" required>
               ${questionList[questionNumber].answer1}
               </label>
               </div>
-              <div class="radios">
+              <div class ='radios'>
               <label for="answer2">
                 <input type="radio" name="answer" id="answer2" required>
               ${questionList[questionNumber].answer2}
               </label>
               </div>
-              <div class="radios">
+              <div class ='radios'>
               <label for="answer3">
                 <input type="radio" name="answer" id="answer3" required>
               ${questionList[questionNumber].answer3}
               </label>
               </div>
-              <div class="radios">
+              <div class ='radios'>
               <label for="answer4">
                 <input type="radio" name="answer" id="answer4" required>
               ${questionList[questionNumber].answer4}
               </label>
               </div>
-              <button type="submit" class="btn btn-success submitButton">Submit</button>
+              <button type="submit" aria-labelledby="submit button" class="btn btn-dark submitButton">Submit</button>
             </fieldset>
           </form>
     `);
     handleSubmitButton();
-    console.log(questionNumber);
+    // console.log(questionNumber);
     }
     else {
       emptyContainer();
@@ -253,59 +253,47 @@ function handleNextButton() {
 }
 
 function finalPage() {
-  console.log('`finalPage` ran');
+  // console.log('`finalPage` ran');
   let finalScore = quizScore;
-  console.log(`User's score: ${quizScore}`);
+  // console.log(`User's score: ${quizScore}`);
   if (finalScore >= 8) {
-    console.log("User's score is greater than or equal to 8");
+    // console.log("User's score is greater than or equal to 8");
     emptyTitle();
     $('.pageInserts').append(`
       <p>Final Score: <span class="quizScore">${finalScore}</span>/10</p>
       <h3> Wowie you're a fan and must drive well too.</h3>
-      <button type="button" class="btn btn-success restartButton">Restart Quiz</button>
+      <button type="button" class="btn btn-dark restartButton">Restart Quiz</button>
     `);
   }
   else if (finalScore < 8 && finalScore >= 5) {
-    console.log("User's score is between 5 and 7");
+    // console.log("User's score is between 5 and 7");
     emptyTitle();
     $('.pageInserts').append(`
       <p>Final Score: <span class="quizScore">${finalScore}</span>/10</p>
       <h3>Nice job get to the track more.</h3>
-      <button type="button" class="btn btn-success restartButton">Restart Quiz</button>
+      <button type="button" class="btn btn-dark restartButton">Restart Quiz</button>
     `);
   }
   else {
-    console.log("User's score is less than or equal to 4");
+    // console.log("User's score is less than or equal to 4");
     emptyTitle();
     $('.pageInserts').append(`
       <p>Final Score: <span class="quizScore">${finalScore}</span>/10</p>
       <h3> Ugh.. You need more track time. :) </h3>
-      <button type="button" class="btn btn-success restartButton">Restart Quiz</button>
+      <button type="button" class="btn btn-dark restartButton">Restart Quiz</button>
     `);
   }
 }
 
 function handleRestartButton() {
   $('.restartButton').on('click', function(event){
-    console.log('`handleRestartButton` ran');
+    // console.log('`handleRestartButton` ran');
     window.location.reload(true);
-    // emptyContainer();
-    // $('.questionScore').append(`
-    //   <li>Question: <span class="questionNumber">0</span>/10</li>
-    //   <li>Score: <span class="quizScore">0</span></li>
-    // `);
-    // $('.startquiz').append(`
-    //   <h2>ARE YOU A TRUE MIGOS FAN?</h2>
-    //   <button type="button" class="startButton">Start</button>
-    // `);
-    // questionNumber = 0;
-    // quizScore = 0;
-    // handleStartButton();
-  });
+    });
 }
 
 function handleAllButtons() {
-  console.log('`handleAllButtons` ran - Starting Quiz');
+  // console.log('`handleAllButtons` ran - Starting Quiz');
   handleStartButton();
   handleSubmitButton();
   handleNextButton();
